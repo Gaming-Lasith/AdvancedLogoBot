fe89c54ee9710d40ee37829055ba1fdd5796cf8f
Gaming Lasith <91444951+Gaming-Lasith@users.noreply.github.com>
2022-08-20 07:52:39 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+API_ID = int(os.getenv("API_ID", ""))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_USERNAME = os.getenv("BOT_USERNAME")
+BOT_NAME = os.getenv("BOT_NAME")

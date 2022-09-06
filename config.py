54252df80ddb3378ffb9963973f36d711ca60157
Gaming Lasith <91444951+Gaming-Lasith@users.noreply.github.com>
2022-09-06 07:00:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
-import os
+from os import environ
 
-API_ID = int(os.getenv("API_ID", ""))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-BOT_USERNAME = os.getenv("BOT_USERNAME")
-BOT_NAME = os.getenv("BOT_NAME")
+class Config(object):
+        API_ID = int(environ.get("API_ID"))
+        API_HASH = environ.get("API_HASH")
+        BOT_TOKEN = environ.get("BOT_TOKEN")
+        BOT_USERNAME = environ.get("BOT_USERNAME")
+        BOT_NAME = environ.get("BOT_NAME")
+        F_SUB_CHANNEL = environ.get("F_SUB_CHANNEL")

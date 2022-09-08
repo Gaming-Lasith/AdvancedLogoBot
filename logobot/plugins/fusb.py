96410a9d0865c1c4e5a312c1d004ece69a538286
Gaming Lasith <91444951+Gaming-Lasith@users.noreply.github.com>
2022-09-08 07:34:38 +0530

    Update fusb.py

--- logobot/plugins/fusb.py
+++ logobot/plugins/fusb.py
@@ -27,7 +27,7 @@ import asyncio
 from pyrogram import Client
 from pyrogram.errors import FloodWait, UserNotParticipant
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
-from config import Config
+from logobot import logobot
 
 CHANNEL_ID = config.F_SUB_CHANNEL
 
